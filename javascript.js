115e680883ddeb020b950634be5153268411657c
codecaviette <63386589+codecaviette@users.noreply.github.com>
2020-12-28 22:22:31 -0800

    Create quote array in JS file

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// Quote array to pull randomly from and display on HTML doc:
+// Array items inherently are zero-based, so first item's index is 0.
+let quotes = [
+    'Sometimes later becomes never. Do it now.',
+    'Push yourself, because no one else is going to do it for you.',
+    'Great things never come from comfort zones.',
+    'Success doesn’t just find you. You have to go out and get it.',
+    'Wake up with determination. Go to bed with satisfaction.',
+    'Do something today that your future self will thank you for.'
+]
