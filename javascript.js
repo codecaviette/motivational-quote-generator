ee4fd9c4366ba7f2c93c9725a9e1abe85bc32fbf
codecaviette <63386589+codecaviette@users.noreply.github.com>
2021-01-13 20:47:14 -0800

    Add backslashes to escape apostrophes in quote string

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ let quotes = [
     'Sometimes later becomes never. Do it now.',
     'Push yourself, because no one else is going to do it for you.',
     'Great things never come from comfort zones.',
-    'Success doesn’t just find you. You have to go out and get it.',
+    'Success doesn\’t just find you. You have to go out and get it.',
     'Wake up with determination. Go to bed with satisfaction.',
     'Do something today that your future self will thank you for.',
     'The key to success is to focus on goals, not obstacles.', 
